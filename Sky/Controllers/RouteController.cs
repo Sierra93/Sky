ac48ee2e692cf21f130885b35ed4fc937c3492ec
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-09-17 23:16:16 +0300

    Добавьте файлы проекта.

---
+++ Sky/Controllers/RouteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sky.Models;
+
+namespace Sky.Controllers {
+    public class RouteController : Controller {
+        private readonly ILogger<RouteController> _logger;
+
+        public RouteController(ILogger<RouteController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sky/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Sky.Models {
+    public class ErrorViewModel {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
