bbd2a09b751f24138353446735f70c7309580cbb
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-09-18 01:29:42 +0300

    Adding header and hover.

--- Sky/Controllers/RouteController.cs
+++ Sky/Controllers/RouteController.cs
@@ -9,23 +9,8 @@ using Sky.Models;
 
 namespace Sky.Controllers {
     public class RouteController : Controller {
-        private readonly ILogger<RouteController> _logger;
-
-        public RouteController(ILogger<RouteController> logger) {
-            _logger = logger;
-        }
-
-        public IActionResult Index() {
+        public IActionResult Index() { 
             return View();
-        }
-
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
